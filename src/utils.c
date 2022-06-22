109fa3214da3cdc834cc26646b43fe29597f72aa
LevoratoJoao <j.vitor.jvlds@gmail.com>
2022-06-22 20:37:23 -0300

    pequenas alteracoes

--- src/utils.c
+++ src/utils.c
@@ -2,30 +2,36 @@
 #include <stdlib.h>
 #include <utils.h>
 
-#define BUFFER_SIZE 32
+#define TAMANHO 10
 
 /* Faz a leitura de uma sequência de caracteres até achar o caracter de parada ou '\n' ou EOF.
  * Durante a leitura, suprime o '\r' caso houver.
- * Aloca em blocos de tamanho BUFFER_SIZE.
- * Caso encontre o EOF, altera 'fimArquivo' para true
+ * Aloca em blocos de tamanho TAMANHO.
+ * Caso encontre o EOF, altera 'fimArquivo' para verdadeiro
  * Adiciona o '\0' no final da string criada.
  * Retorna a string.
  */
 char *lerString(FILE *f, char parada) {
-    char *linha = NULL, tmp;
-    int cont = 0;
+    char *linha = NULL, tmp; //variavel temporaria
+    int cont = 0; //contador
 
     do {
         tmp = getc(f);
-        if (tmp == '\r') tmp = getc(f);
+        if (tmp == '\r') {
+            tmp = getc(f);
+        }
 
-        if (cont % BUFFER_SIZE == 0) // Aloca em blocos de tamanho BUFFER_SIZE
-            linha = realloc(linha, sizeof(char) * (cont + BUFFER_SIZE));
+        if (cont % TAMANHO == 0) { // Aloca em blocos de tamanho TAMANHO
+            linha = realloc(linha, sizeof(char) * (cont + TAMANHO));
+        }
 
-        if (tmp == parada) break;
+        if (tmp == parada) {
+            break;
+        }
 
-        if ((tmp != '\n') && (tmp != EOF))
+        if ((tmp != '\n') && (tmp != EOF)) {
             linha[cont++] = tmp;
+        }
 
     } while ((tmp != '\n') && (tmp != EOF));
 
